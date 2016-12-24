4a67412915975d6e9b666b8c2fbc13acf209075e
seveibar <seveibar@gmail.com>
2016-12-24 15:23:02 -0500

    added cities

--- client/src/containers/GamePage/index.js
+++ client/src/containers/GamePage/index.js
@@ -10,7 +10,7 @@ const GamePage = (props) => {
           <Cell x={50} y={10} owner={1} force={10} type={"king"} />
           <Cell x={200} y={10} owner={0} force={0} type={"mountain"} />
           <Cell x={350} y={10} owner={2} force={50} type={"land"} />
-          <Cell x={500} y={10} owner={3} force={50} type={"city"} />
+          <Cell x={500} y={10} owner={3} force={0} type={"city"} />
         </Layer>
       </Stage>
     </div>
